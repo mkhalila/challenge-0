2c05347260ac38f09a68dab624a0c56095dd0b31
k1502755 <mohamad.ahmed@kcl.ac.uk>
2016-09-26 17:30:08 +0100

    Added skeleton logic of method without implementations of edits

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -1,10 +1,34 @@
 public class SingleCharacterEdit {
+	private boolean editMade;
 
     public static String singleCharacterEditWillTurnAIntoB(String a, String b) {
         if ( (Math.abs( a.length() - b.length() ) < 2) && (!a.equals(b)) ) {
-        	
+        	editMade = false;
+        	while(editMade == false) {
+        		if (a.length() == b.length()) {
+        			return replace(a, b);
+        		}
+        		else if (a.length() > b.length()) {
+        			return remove(a, b);
+        		}
+        		else {
+        			return insert(a, b);
+        		}
         }
         return null; // this is only here to make it compile
     }
 
+    private String replace(String a, String b) {
+    	return null;
+    }
+
+    private String remove(String a, String b) {
+    	return null;
+    }
+
+    private String insert(String a, String b) {
+    	return null;
+    }
+
 }
+
