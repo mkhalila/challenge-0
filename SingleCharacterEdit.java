995d8bc849e7db24cc31a760dcc3061833407f06
k1502755 <mohamad.ahmed@kcl.ac.uk>
2016-09-26 17:01:14 +0100

    Added String.equal() check

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -1,7 +1,7 @@
 public class SingleCharacterEdit {
 
     public static String singleCharacterEditWillTurnAIntoB(String a, String b) {
-        if (Math.abs( a.length() - b.length() ) < 2) {
+        if ( (Math.abs( a.length() - b.length() ) < 2) && (!a.equals(b)) ) {
         	
         }
         return null; // this is only here to make it compile
