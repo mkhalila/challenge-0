9b2877a09a69fef797729acf17d23c082432daad
Ahmed, Mohamad (k1502755) <mohamad.ahmed@kcl.ac.uk>
2016-09-26 15:27:05 +0100

    Add files via upload

---
+++ ScreenAsBytes.java
@@ -0,0 +1,16 @@
+public class ScreenAsBytes {
+
+    public static void setPixel(byte[] screen, int width, int x, int y) {
+        // TODO complete this code
+    }
+
+    public static void drawHorizontalLine(byte[] screen, int width, int startX, int endX, int y) {
+        // TODO complete this code
+    }
+    
+    public static void drawVerticalLine(byte[] screen, int width, int x, int startY, int endY) {
+        // TODO complete this code
+    }
+
+
+}
---
+++ SingleCharacterEdit.java
@@ -0,0 +1,8 @@
+public class SingleCharacterEdit {
+
+    public static String singleCharacterEditWillTurnAIntoB(String a, String b) {
+        // TODO: your code goes here, replacing the next line with your actual code
+        return null; // this is only here to make it compile
+    }
+
+}
