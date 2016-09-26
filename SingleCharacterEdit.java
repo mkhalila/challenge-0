462df073f9e29b765949987eb8312272deb23eb8
k1502755 <mohamad.ahmed@kcl.ac.uk>
2016-09-26 16:46:03 +0100

    Added intial length check to see if single char edit is possible

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -1,7 +1,9 @@
 public class SingleCharacterEdit {
 
     public static String singleCharacterEditWillTurnAIntoB(String a, String b) {
-        // TODO: your code goes here, replacing the next line with your actual code
+        if (Math.abs( a.length() - b.length() ) < 2) {
+        	
+        }
         return null; // this is only here to make it compile
     }
 
