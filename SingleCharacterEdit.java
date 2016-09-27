f0297656b372d27552abacae0f7d23be02334fd9
k1502755 <mohamad.ahmed@kcl.ac.uk>
2016-09-27 09:56:06 +0100

    Implemented code for remove() method

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -30,6 +30,12 @@ public class SingleCharacterEdit {
     }
 
     private static String remove(String a, String b) {
+    	for (int i = 0; i < a.length(); i++) {
+    		if (a.charAt(i) != b.charAt(i)) {
+    			editMade = true;
+    			return "remove," + i;
+    		}
+    	}
     	return null;
     }
 
