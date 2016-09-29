fa7ac215165bf1c30de70f35d4c96b6845cef45c
k1502755 <mohamad.ahmed@kcl.ac.uk>
2016-09-29 20:07:44 +0100

    Re-commit

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -1,32 +1,32 @@
 public class SingleCharacterEdit {
-	private static boolean editMade;
+    private static boolean editMade;
 
     public static String singleCharacterEditWillTurnAIntoB(String a, String b) {
         if ( (Math.abs( a.length() - b.length() ) < 2) && (!a.equals(b)) ) {
-        	editMade = false;
-        	while(editMade == false) {
-        		if (a.length() == b.length()) {
-        			return replace(a, b);
-        		}
-        		else if (a.length() > b.length()) {
-        			return remove(a, b);
-        		}
-        		else {
-        			return insert(a, b);
-        		}
-        	}
+            editMade = false;
+            while(editMade == false) {
+                if (a.length() == b.length()) {
+                    return replace(a, b);
+                }
+                else if (a.length() > b.length()) {
+                    return remove(a, b);
+                }
+                else {
+                    return insert(a, b);
+                }
+            }
         }
         return null; // this is only here to make it compile
     }
 
     private static String replace(String a, String b) {
-    	for (int i = 0; i < a.length(); i++) {
-    		if (a.charAt(i) != b.charAt(i)) {
-    			editMade = true;
-    			return "replace," + i + "," + b.charAt(i);
-    		}
-    	}
-    	return null;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                editMade = true;
+                return "replace," + i + "," + b.charAt(i);
+            }
+        }
+        return null;
     }
 
     private static String remove(String a, String b) {
@@ -34,13 +34,13 @@ public class SingleCharacterEdit {
             editMade = true;
             return "remove," + (a.length()-1);
         }
-    	for (int i = 0; i < a.length(); i++) {
-    		if (a.charAt(i) != b.charAt(i)) {
-    			editMade = true;
-    			return "remove," + i;
-    		}
-    	}
-    	return null;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                editMade = true;
+                return "remove," + i;
+            }
+        }
+        return null;
     }
 
     private static String insert(String a, String b) {
@@ -54,8 +54,7 @@ public class SingleCharacterEdit {
             editMade = true;
             return "insert," + a.length() + "," + b.charAt(b.length()-1);
         }
-    	return null;
+        return null;
     }
 
 }
-
