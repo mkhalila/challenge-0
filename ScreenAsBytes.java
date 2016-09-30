ef17259e9b7520fbfc85d1d6cb8c72e8f68f4694
k1502755 <mohamad.ahmed@kcl.ac.uk>
2016-10-01 00:14:40 +0100

    Removed testing and debugging code

--- ScreenAsBytes.java
+++ ScreenAsBytes.java
@@ -2,21 +2,13 @@ public class ScreenAsBytes {
 
     public static void setPixel(byte[] screen, int width, int x, int y) {
         int bitsPerRow = width/8;
-        System.out.println("bits per row " + bitsPerRow);
         int jumpToBit = bitsPerRow*y;
-        System.out.println("jum to bit " + jumpToBit);
         if (x > 7) {
         	int xdiv8 = x/8;
         	x -= 8 * xdiv8;
-        	System.out.println("x = " + x);
         	jumpToBit += xdiv8;
-        	System.out.println("jumpToBit " + jumpToBit);
         }
         screen[jumpToBit] |= (1 << 7-x);
-        System.out.println("new value: " + screen[jumpToBit]);
-        for (byte b : screen ) {
-        	System.out.println(b);
-        }
     }
 
     public static void drawHorizontalLine(byte[] screen, int width, int startX, int endX, int y) {
@@ -27,8 +19,4 @@ public class ScreenAsBytes {
         // TODO complete this code
     }
 
-    public static void main(String[] args) {
-    	byte[] screen = new byte[12];
-    	setPixel(screen, 32, 22, 2);
-    }
 }
